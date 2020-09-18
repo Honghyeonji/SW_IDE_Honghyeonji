a597234c8e36a1451a72746ab6138b969f2e9ffc
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2020-09-18 13:13:17 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,37 +1,38 @@
-#define PIN_LED 13
+#define PIN_LED 17
 unsigned int count, toggle;
 
 void setup() {
   pinMode(PIN7, OUTPUT);
-  Serial.begin(4800); // Initialize serial port
+  Serial.begin(9600); // Initialize serial port
   while (!Serial) {
     ; // wait for serial port to connect.
   }
   Serial.println("Hello World!");
-  count = 0;
-  toggle = 0;
-  digitalWrite(PIN7, toggle);
+  count = toggle = 0;
+  digitalWrite(PIN7, toggle); //turn on LED
 }
-
 
 void loop() {
   if(count == 0){
     Serial.println(count);
     digitalWrite(PIN7, toggle);
+    count += 1;
     delay(1000);
   }
-  
-  if(0< count < 10){
-    Serial.println(++count);
-    toggle = toggle_state(toggle);
-    digitalWrite(PIN7, toggle);
+
+  for (int i = 0; i < 10;  i++){
+    if(count == 1){ 
+      Serial.println(count); }
+    toggle = toggle_state(toggle); 
+    digitalWrite(PIN7, toggle); 
+    count += 1;
     delay(100);
   }
-  
-  if (count >= 10){
-    Serial.println(++count);
-    toggle = toggle_state(toggle);
-    digitalWrite(PIN7,toggle);
+
+  if(count >= 11){
+    Serial.println(2);
+    toggle = toggle_state(toggle); 
+    digitalWrite(PIN7, toggle); 
     while(1){}
   }
 }
